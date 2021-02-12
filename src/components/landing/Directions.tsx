f6b37290c81b409862bddf92412b9729a5c295b3
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-12 15:27:24 -0600

    Code refactor

--- src/components/landing/Directions.tsx
+++ src/components/landing/Directions.tsx
@@ -3,49 +3,47 @@ import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LearnMoreCard } from '../LearnMoreCard';
 import { faGamepad, faUsers, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-
-const cards = {
-    compete: {
-        title: "Соревнования",
-        text: "Турниры по ключевым и экспериментальным киберспортивным дисциплинам.",
-        link: "",
-        footerColor: "#37C794",
-    },
-    career: {
-        title: "Профессия",
-        text: "Развитие профессиональных навыков и карьерные возможности для каждого студента.",
-        link: "",
-        footerColor: "#DC3545",
-    },
-    friendship: {
-        title: "Дружественные связи",
-        text: "Возможности найти единомышленников, товарищей и близких друзей на всю жизнь.",
-        link: "",
-        footerColor: "#37A4C7",
-    }
-}
+import { useState } from 'react';
 
 export const Directions: FC = () => {
+  const [cards] = useState([
+    {
+      title: 'Соревнования',
+      text: 'Турниры по ключевым и экспериментальным киберспортивным дисциплинам.',
+      link: '',
+      footerColor: '#37C794',
+      icon: faGamepad,
+    },
+    {
+      title: 'Профессия',
+      text: 'Развитие профессиональных навыков и карьерные возможности для каждого студента.',
+      link: '',
+      footerColor: '#DC3545',
+      icon: faPaperPlane,
+    },
+    {
+      title: 'Дружественные связи',
+      text: 'Возможности найти единомышленников, товарищей и близких друзей на всю жизнь.',
+      link: '',
+      footerColor: '#37A4C7',
+      icon: faUsers,
+    },
+  ]);
   return (
     <Row className='d-flex align-items-center mt-5'>
       <Col xs='12'>
         <h2 className='title-2 pl-lg-5'>Сферы деятельности</h2>
       </Col>
-      <Col>
-        <LearnMoreCard cardTitle={cards.compete.title} cardText={cards.compete.text} footerColor={cards.compete.footerColor}>
-          <FontAwesomeIcon icon={faGamepad} style={{ color: '#37C794', height: '32px', width: '32px' }} />
-        </LearnMoreCard>
-      </Col>
-      <Col>
-        <LearnMoreCard cardTitle={cards.career.title} cardText={cards.career.text} footerColor={cards.career.footerColor}>
-            <FontAwesomeIcon icon={faPaperPlane} style={{ color: '#DC3545', height: '32px', width: '32px' }} />
-        </LearnMoreCard>
-      </Col>
-      <Col>
-        <LearnMoreCard cardTitle={cards.friendship.title} cardText={cards.friendship.text} footerColor={cards.friendship.footerColor}>
-            <FontAwesomeIcon icon={faUsers} style={{ color: '#37A4C7', height: '32px', width: '32px' }} />
-        </LearnMoreCard>
-      </Col>
+      {cards.map((card) => (
+        <Col>
+          <LearnMoreCard cardTitle={card.title} cardText={card.text} footerColor={card.footerColor}>
+            <FontAwesomeIcon
+              icon={card.icon}
+              style={{ color: card.footerColor, height: '32px', width: '32px' }}
+            />
+          </LearnMoreCard>
+        </Col>
+      ))}
     </Row>
   );
 };
--- src/components/landing/Partners.tsx
+++ src/components/landing/Partners.tsx
@@ -1,22 +1,24 @@
 import React, { FC } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import styled from '@emotion/styled/macro';
 
 import { ReactComponent as Logo } from '../../images/logo_sm.svg';
-import vsuLogo from "../../images/vsuesportsblue.png"
-import gaugnLogo from "../../images/gaugn.png"
+import vsuLogo from '../../images/vsuesportsblue.png';
+import gaugnLogo from '../../images/gaugn.png';
+
+const PartnerLogo = styled.img`
+  width: 70px;
+  height: 70px;
+`;
 
 export const Partners: FC = () => {
-    return (
-        <Row className="d-flex align-items-center mt-3">
-            <Col className="d-flex align-items-center justify-content-center">
-                <Logo />
-                <Logo />
-                <Logo />
-                <Logo />
-                <Logo />
-                <img style={{width: '70px', height: '70px'}} src={gaugnLogo} alt="vsu"/>
-                <img style={{width: '70px', height: '70px'}} src={vsuLogo} alt="vsu"/>
-            </Col>
-        </Row>
-    );
+  return (
+    <Row className='d-flex align-items-center mt-3'>
+      <Col className='d-flex align-items-center justify-content-center'>
+        <Logo />
+        <PartnerLogo src={gaugnLogo} alt='vsu' />
+        <PartnerLogo src={vsuLogo} alt='vsu' />
+      </Col>
+    </Row>
+  );
 };
