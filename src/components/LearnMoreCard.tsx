b1cf344c51eab1ba5b40edb24af46a6d6b9c80b2
Evgeniy Petrovich <evgeniy.petrovich@skillbox.ru>
2021-02-13 00:12:18 +0300

    features block

--- src/components/LearnMoreCard.tsx
+++ src/components/LearnMoreCard.tsx
@@ -1,10 +1,24 @@
 import React, { FC } from "react";
 import { Card } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowAltRight } from '@fortawesome/free-solid-svg-icons';
 
-export const LearnMoreCard: FC = ({ children }) => {
+export const LearnMoreCard: FC<
+{
+    cardTitle: string;
+    cardText: string;
+    footerColor: string;
+}> = ({ cardTitle, cardText, footerColor, children }) => {
   return (
-    <Card bg="dark">
+    <Card bg="dark" text="light" className="mt-5">
       <Card.Header>{children}</Card.Header>
+      <Card.Body>
+        <Card.Title style={{ fontWeight: 'normal' }}>{cardTitle}</Card.Title>
+        <Card.Text style={{ fontWeight: 'lighter' }}>{cardText}</Card.Text>
+        <Card.Footer color={footerColor} className="d-flex align-items-center">
+            Узнать больше  <FontAwesomeIcon className="ml-2" icon={faLongArrowAltRight} style={{ color: footerColor, height: '20px', width: '20px' }} />
+        </Card.Footer>
+      </Card.Body>
     </Card>
   );
 };
--- src/components/landing/Directions.tsx
+++ src/components/landing/Directions.tsx
@@ -2,24 +2,49 @@ import React, { FC } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LearnMoreCard } from '../LearnMoreCard';
-import { faGamepad } from '@fortawesome/free-solid-svg-icons';
+import { faGamepad, faUsers, faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+const cards = {
+    compete: {
+        title: "Соревнования",
+        text: "Турниры по ключевым и экспериментальным киберспортивным дисциплинам.",
+        link: "",
+        footerColor: "#37C794",
+    },
+    career: {
+        title: "Профессия",
+        text: "Развитие профессиональных навыков и карьерные возможности для каждого студента.",
+        link: "",
+        footerColor: "#DC3545",
+    },
+    friendship: {
+        title: "Дружественные связи",
+        text: "Возможности найти единомышленников, товарищей и близких друзей на всю жизнь.",
+        link: "",
+        footerColor: "#37A4C7",
+    }
+}
 
 export const Directions: FC = () => {
   return (
     <Row className='d-flex align-items-center mt-5'>
       <Col xs='12'>
-        <h2 className='title-2 pl-5'>Сферы деятельности</h2>
+        <h2 className='title-2 pl-lg-5'>Сферы деятельности</h2>
       </Col>
       <Col>
-        <LearnMoreCard>
-          <FontAwesomeIcon icon={faGamepad} style={{ color: 'white' }} />
+        <LearnMoreCard cardTitle={cards.compete.title} cardText={cards.compete.text} footerColor={cards.compete.footerColor}>
+          <FontAwesomeIcon icon={faGamepad} style={{ color: '#37C794', height: '32px', width: '32px' }} />
         </LearnMoreCard>
       </Col>
       <Col>
-        <LearnMoreCard></LearnMoreCard>
+        <LearnMoreCard cardTitle={cards.career.title} cardText={cards.career.text} footerColor={cards.career.footerColor}>
+            <FontAwesomeIcon icon={faPaperPlane} style={{ color: '#DC3545', height: '32px', width: '32px' }} />
+        </LearnMoreCard>
       </Col>
       <Col>
-        <LearnMoreCard></LearnMoreCard>
+        <LearnMoreCard cardTitle={cards.friendship.title} cardText={cards.friendship.text} footerColor={cards.friendship.footerColor}>
+            <FontAwesomeIcon icon={faUsers} style={{ color: '#37A4C7', height: '32px', width: '32px' }} />
+        </LearnMoreCard>
       </Col>
     </Row>
   );
