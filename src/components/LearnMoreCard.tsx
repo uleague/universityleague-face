b573f67ac2212012a305390e450091c1009f6752
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-12 13:55:02 -0600

    Children fix

--- src/components/LearnMoreCard.tsx
+++ src/components/LearnMoreCard.tsx
@@ -1,17 +1,17 @@
-import React, { Children, FC } from "react";
+import React, { FC } from 'react';
 import { Card } from 'react-bootstrap';
 import { IconContext } from 'react-icons';
 
-export const LearnMoreCard: FC<{ 
-    iconColorVar: string;
-}> = ({ iconColorVar }) => {
+export const LearnMoreCard: FC<{
+  iconColorVar: string;
+}> = ({ iconColorVar, children }) => {
   return (
-    <Card bg="dark">
-        <Card.Header>
-            <IconContext.Provider value={{ color: iconColorVar, size: '50px' }} >
-               {Children}
-            </IconContext.Provider>
-        </Card.Header>
+    <Card bg='dark'>
+      <Card.Header>
+        <IconContext.Provider value={{ color: iconColorVar, size: '50px' }}>
+          {children}
+        </IconContext.Provider>
+      </Card.Header>
     </Card>
   );
 };
