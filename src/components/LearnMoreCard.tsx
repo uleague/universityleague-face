048fe6bde831ee004382c0d44d247fdc6eef2c07
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-12 14:28:14 -0600

    Fix icon

--- src/components/LearnMoreCard.tsx
+++ src/components/LearnMoreCard.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { FC } from "react";
+import { Card } from "react-bootstrap";
 
 export const LearnMoreCard: FC = ({ children }) => {
   return (
-    <Card bg='dark'>
-      <Card.Header>
-          {children}
-      </Card.Header>
+    <Card bg="dark">
+      <Card.Header>{children}</Card.Header>
     </Card>
   );
 };
--- src/components/landing/Directions.tsx
+++ src/components/landing/Directions.tsx
@@ -1,28 +1,26 @@
 import React, { FC } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
-import { LearnMoreCard } from '../LearnMoreCard'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { LearnMoreCard } from '../LearnMoreCard';
+import { faGamepad } from '@fortawesome/free-solid-svg-icons';
 
 export const Directions: FC = () => {
   return (
-    <Row className="d-flex align-items-center mt-5">
-        <Col xs="12">
-            <h2 className="title-2 pl-5">Сферы деятельности</h2>
-        </Col>
-        <Col>
-            <LearnMoreCard>
-                <FontAwesomeIcon icon={["fas", "gamepad"]} />
-            </LearnMoreCard>
-        </Col>
-        <Col>
-            <LearnMoreCard>
-            </LearnMoreCard>
-        </Col>
-        <Col>
-            <LearnMoreCard>
-            </LearnMoreCard>
-        </Col>
+    <Row className='d-flex align-items-center mt-5'>
+      <Col xs='12'>
+        <h2 className='title-2 pl-5'>Сферы деятельности</h2>
+      </Col>
+      <Col>
+        <LearnMoreCard>
+          <FontAwesomeIcon icon={faGamepad} style={{ color: 'white' }} />
+        </LearnMoreCard>
+      </Col>
+      <Col>
+        <LearnMoreCard></LearnMoreCard>
+      </Col>
+      <Col>
+        <LearnMoreCard></LearnMoreCard>
+      </Col>
     </Row>
   );
 };
