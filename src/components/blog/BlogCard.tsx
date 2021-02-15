8b16101c82c9c598f2b21d290a408b8c2a600232
Evgeniy Petrovich <evgeniy.petrovich@skillbox.ru>
2021-02-15 23:41:50 +0300

    blog cards responsive work

--- src/components/blog/BlogCard.tsx
+++ src/components/blog/BlogCard.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Col } from 'react-bootstrap';
 import styled from '@emotion/styled/macro';
 import { Link } from 'react-router-dom';
 
@@ -22,16 +22,18 @@ export const BlogCard: FC<{
   title: string;
 }> = ({ tag, img, title }) => {
   return (
-    <Link
-      to='#'
-      style={{ cursor: 'pointer', textDecoration: 'none' }}
-      className='d-flex flex-column justify-content-start col-4 pb-4'
-    >
-      <Card style={{ backgroundColor: '#292B36', border: 'none' }}>
-        {tag && <CardTag>{tag}</CardTag>}
-        <Card.Img variant='top' src={img} style={{ height: '250px', borderRadius: '20px' }} />
-        <CardTitle>{title}</CardTitle>
-      </Card>
-    </Link>
+    <Col md="4" sm="12">
+      <Link
+        to='#'
+        style={{ cursor: 'pointer', textDecoration: 'none' }}
+        className='d-flex flex-column justify-content-start pb-4'
+      >
+        <Card style={{ backgroundColor: '#292B36', border: 'none' }}>
+          {tag && <CardTag>{tag}</CardTag>}
+          <Card.Img variant='top' src={img} style={{ height: '250px', width: '350px', borderRadius: '20px' }} />
+          <CardTitle>{title}</CardTitle>
+        </Card>
+      </Link>
+    </Col>
   );
 };
