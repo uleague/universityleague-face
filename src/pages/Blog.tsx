95535e8507998bc27d73d8736dc064c59ed0505f
semenodp <dmitry.p.semenov@vanderbilt.edu>
2021-02-26 13:22:08 -0600

    Fix use params

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,19 +1,12 @@
-import { Container, Col, Row } from "react-bootstrap";
-import React, { FC } from "react";
-import {
-  useParams,
-  Switch,
-  Route
-} from "react-router-dom";
-
+import { Container, Col, Row } from 'react-bootstrap';
+import React, { FC } from 'react';
+import { useParams, Switch, Route } from 'react-router-dom';
 
 export const Blog: FC = () => {
-  let { blogId } = useParams();
+  let { blogId } = useParams<{ blogId: string }>();
   return (
-    <Container fluid="lg" className="pt-4">
-      <Row>
-        {blogId}
-      </Row>
+    <Container fluid='lg' className='pt-4'>
+      <Row>{blogId}</Row>
     </Container>
   );
 };
